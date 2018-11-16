3b06684f56803edd349ab5efc56c8e48b9ca177e
KuKKilicious <clouseyda@yahoo.de>
2018-11-16 22:09:11 +0100

    Splash Screen and Standard Assets import

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+    [SerializeField]
+    int waitForSeconds = 4;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(LoadNextLevel());
+	}
+	
+	private IEnumerator LoadNextLevel() {
+        yield return new WaitForSeconds(waitForSeconds);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
