d332dbd35618ccc2cd58fa34f5f67090ddda75d2
KuKKilicious <clouseyda@yahoo.de>
2018-11-19 01:23:54 +0100

    added Player ship pitch yaw,roll

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,36 +7,61 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
     [Tooltip("In ms^-1")]
     [SerializeField]
-    float xMovementSpeed = 5f;
+    float xMovementSpeed = 16f;
 
     [Tooltip("In ms^-1")]
     [SerializeField]
-    float yMovementSpeed = 5f;
-    // Use this for initialization
-    void Start() {
+    float yMovementSpeed = 9f;
+    //Values for Mathf.Clamp to prohi
+    [Tooltip("local position Boundary")]
+    [SerializeField]
+    float xClampMinMax = 8.8f;
+    [Tooltip("local position Boundary")]
+    [SerializeField]
+    float yClampMinMax = 4.7f;
 
-    }
+    [SerializeField]
+    float positionPitchFactor = -3f;
+    [SerializeField]
+    float controlPitchFactor = -8f;
+    [SerializeField]
+    float positionYawFactor = 2f;
+    [SerializeField]
+    float positionRollFactor = -20f;
 
+    float horizontalThrow, verticalThrow;
     // Update is called once per frame
     void Update() {
+
+        
+        //Process Translation
         HandleMovement();
+        //Process Rotation
+        AdjustRotation();
     }
 
     private void HandleMovement() {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = horizontalThrow * xMovementSpeed * Time.deltaTime;
         float yOffset = verticalThrow * yMovementSpeed * Time.deltaTime;
 
-        print(xOffset);
-        /* if (horizontalThrow > Mathf.Epsilon) {
-             transform.position =
-         }
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
 
-         if (verticalThrow > Mathf.Epsilon) {
-             transform.position = 
-         }
-         */
+        float clampedNewXPos = Mathf.Clamp(rawNewXPos, -1 * xClampMinMax, xClampMinMax);
+        float clampedNewYPos = Mathf.Clamp(rawNewYPos, -1 * yClampMinMax, yClampMinMax);
+
+        transform.localPosition = new Vector3(clampedNewXPos, clampedNewYPos, transform.localPosition.z);
+
     }
+
+    private void AdjustRotation() {
+        float pitch = transform.localPosition.y * positionPitchFactor + verticalThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = horizontalThrow * positionRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+    }
+
 }
