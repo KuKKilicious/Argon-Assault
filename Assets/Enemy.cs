d1bd6acc70e802017cb490fb6caa39e839e5a59d
KuKKilicious <clouseyda@yahoo.de>
2018-11-24 16:23:42 +0100

    added Score Board, destroying enemy ship functionality

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField]
+    GameObject deathFX;
+    [SerializeField]
+    Transform parent;
+    [SerializeField]
+    int scoreWorth;
+	// Use this for initialization
+	void Start () {
+        AddNonTriggerBoxCollider(); 
+        
+	}
+
+    private void AddNonTriggerBoxCollider() {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other) {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -6,7 +6,12 @@ public class MusicPlayer : MonoBehaviour {
 
 
     private void Awake() {
+        int numMusicPlayer = GameObject.FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayer >1) {
+            Destroy(gameObject);
+        }else {
         DontDestroyOnLoad(gameObject);
+        }
     }
    
 }
